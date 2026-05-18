a5677afab749c576b8a52838ffff48d305253630
scrollshadow <zaldhypramana@gmail.com>
2026-05-18 15:05:49 +0700

    3

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -9,7 +9,7 @@ public:
     void setSaklarNo(int i, string value) {
         saklarNo[i] = value;
     }
-    string getSaklarNo(int i) {
+    string getSaklarNo(int i) { //variabel array lampu
         return saklarNo[i];
     }
 };
